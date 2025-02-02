274f1b4a854e7120f838b9944b602f44b13491a8
telferi <telferenc@gmail.com>
2025-02-02 09:37:58 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-
+// script.js
+jQuery(document).ready(function($) {
+    // Példa Stripe integráció
+    $('#ebook-donation-stripe-btn').on('click', function(e) {
+        e.preventDefault();
+        const amount = $(this).data('amount');
+        // Itt kell meghívni a Stripe fizetési ablakát vagy PaymentIntent logic-ot
+        alert('Stripe fizetés indulna: ' + amount + ' USD');
+    });
+});
